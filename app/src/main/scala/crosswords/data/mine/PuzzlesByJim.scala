20dc7f6bf5797a6b31639e9d1299b0b38da0c078
Jojo le Barjos <jojolebarjos@gmail.com>
2015-03-12 17:34:09 +0100

    finished puzzle by jim data miner

--- app/src/main/scala/crosswords/data/mine/PuzzlesByJim.scala
+++ app/src/main/scala/crosswords/data/mine/PuzzlesByJim.scala
@@ -16,17 +16,26 @@ import scala.xml.{NodeSeq, Elem, XML}
  */
 object PuzzlesByJim {
 
+  val website = "http://www.puzzlesbyjim.com/"
+
   private val _begin = "var CrosswordPuzzleData = \""
   private val _end = "\";"
 
-  private def parse(raw: String, id: Int): JsObject = {
-    
-    // TODO it seems that this is the format of http://www.crossword-compiler.com/
+  /**
+   * Parse crossword in <a href="http://www.crossword-compiler.com/">Crossword Compiler</a> format.
+   * Returned JSON crossword does only have the <code>words</code> attribute.
+   *
+   * @param js JS file content
+   * @return a JSON representation of the crossword
+   */
+  def parse(js: String): JsObject = {
+
+    // TODO is it really the Crossword Compiler format?
 
     // Extract XML code
-    val begin = raw.indexOf(_begin)
-    val end = raw.lastIndexOf(_end)
-    val content = raw.substring(begin + _begin.size, end)
+    val begin = js.indexOf(_begin)
+    val end = js.lastIndexOf(_end)
+    val content = js.substring(begin + _begin.size, end)
     val xml = XML.loadString(content.replace("\\\"", "\""))
     val crossword = xml \\ "crossword"
 
@@ -37,7 +46,7 @@ object PuzzlesByJim {
       (c \ "@number").text,
       (c \ "@solution").text,
       Vec((c \ "@x").text.toInt, (c \ "@y").text.toInt)
-    )).toSeq
+      )).toSeq
     val grid = cells.map(c => c._3 -> (if (c._1.isEmpty) None else Some(c._1.toInt), c._2.headOption)).
       toMap.withDefaultValue((None, None))
     val locations = grid.filter(_._2._1.isDefined).map(_._1).flatMap(v =>
@@ -56,16 +65,11 @@ object PuzzlesByJim {
     def extract(dir: Direction, seq: NodeSeq) = seq.map(c => (
       (dir, (c \ "@number").text.toInt),
       ((c \ "@word").text.toInt, c.text)
-    )).toMap
+      )).toMap
     val clues = extract(East, list.take(split)) ++ extract(South, list.drop(split + 1))
 
     // Build JSON
     Json.obj(
-      "source" -> "Puzzles by Jim",
-      "language" -> "eng",
-      "url" -> ("http://www.puzzlesbyjim.com/challenge--" + id + ".html"),
-      "title" -> ("Challenge #" + id),
-      "author" -> "Jim",
       "words" -> words.map{case (k@(d, _), (w, v)) => Json.obj(
         "word" -> w,
         "clue" -> clues(k)._2,
@@ -77,18 +81,49 @@ object PuzzlesByJim {
 
   }
 
-  private val _root = "http://www.puzzlesbyjim.com/JS/challenge_0002.js"
+  private def enumerate() = {
+
+    // Get Puzzle by Jim homepage
+    val content = Source.fromURL(website).getLines().mkString
+
+    // Find all entries
+    val regex = """<a href="([^"]*)">\s*<span class="imMnMnBorder">\s*<span class="imMnMnTxt"><span class="imMnMnImg"></span>([^<]*)</span>\s*</span>\s*</a>""".r
+    regex.findAllMatchIn(content).map(m => (website + m.group(1), m.group(2))).toVector
+    // TODO unescape HTML in title
+
+  }
+
+  private def getJsUrl(url: String) = {
+
+    // Get puzzle HTML file
+    val content = Source.fromURL(url).getLines().mkString
+
+    // Find JS url
+    val regex = """src="CrosswordCompilerApp/crosswordCompiler.js"></script><script src="([^"]*)"></script>""".r
+    website + regex.findFirstMatchIn(content).map(_.group(1)).get
+
+  }
+
+  private def write(file: File, content: String) {
+    val out = new FileWriter(file)
+    out.write(content)
+    out.close()
+  }
+
   private val _folder = "../data/jim/"
 
-  private def query(id: Int): Boolean = {
+  private def query(url: String, title: String) {
 
-    // Do nothing if there already is the JSON file
-    val jsonFile = new File(_folder + id + ".json")
+    // Get safe name
+    val name = title.replaceAll("[^A-Za-z0-9 ]", "").trim
+
+    // Check if JSON file already exists
+    val jsonFile = new File(_folder + name + ".json")
     if (jsonFile.exists())
-      return false
+      return
 
     // Check if the JS file exists
-    val jsFile = new File(_folder + id + ".js")
+    val jsFile = new File(_folder + name + ".js")
     if (!jsFile.exists()) {
 
       // Guarantee that folder is here
@@ -96,40 +131,45 @@ object PuzzlesByJim {
         throw new IOException("failed to create directory for Puzzles by Jim")
 
       // Download HTML file
-      println("downloading " + id + "...")
-      val url = "http://www.puzzlesbyjim.com/JS/challenge_" + "%04d".format(id) + ".js"
-      val js = try Source.fromURL(url).mkString catch { case e: Exception =>
-        System.err.println("failed to wget " + id + " (" + e.getMessage + ")")
+      println("downloading " + title + "...")
+      val js = try Source.fromURL(getJsUrl(url)).mkString catch { case e: Exception =>
+        System.err.println("failed to wget " + title + " (" + e.getMessage + ")")
         ""
       }
 
       // Save content
-      val out = new FileWriter(jsFile)
-      out.write(js)
-      out.close()
+      write(jsFile, js)
 
     }
 
     // Convert to JSON
-    val json = try Json.prettyPrint(parse(Source.fromFile(jsFile).mkString, id)) catch { case e: Exception =>
-      System.err.println("failed to parse " + id + " (" + e.getMessage + ")")
+    val json = try {
+      var json = Json.obj()
+      json += "source" -> Json.toJson("Puzzles by Jim")
+      json += "url" -> Json.toJson(url)
+      json += "title" -> Json.toJson(title)
+      json += "author" -> Json.toJson("Jim")
+      json += "language" -> Json.toJson("eng")
+      json ++= parse(Source.fromFile(jsFile).mkString)
+      Json.prettyPrint(json)
+    } catch { case e: Exception =>
+      System.err.println("failed to parse " + title + " (" + e.getMessage + ")")
       ""
     }
 
     // Save content
-    val out = new FileWriter(jsonFile)
-    out.write(json)
-    out.close()
+    write(jsonFile, json)
 
-    println(id + " successfully exported!")
-    true
+    // Success
+    println("Challenge " + title + " successfully exported!")
 
   }
 
   def main(args: Array[String]) {
 
-    for (id <- 1 to 222)
-      query(id)
+    val items = enumerate()
+    for ((url, title) <- items)
+      query(url, title)
 
   }
 
