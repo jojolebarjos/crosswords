d5791c0ea8e3b4aba14683918d034bd24fe02002
mfilippo <matteo.filipponi@epfl.ch>
2015-04-18 09:39:47 +0200

    Vector Space modifications

--- app/src/main/scala/crosswords/spark/VectorSpaceTest.scala
+++ app/src/main/scala/crosswords/spark/VectorSpaceTest.scala
@@ -2,23 +2,23 @@ package crosswords.spark
 
 import java.io.{File, BufferedWriter, FileWriter}
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import JsonInputFormat._
 import play.api.libs.json.JsObject
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.feature.{HashingTF, IDF, Normalizer}
+import org.apache.spark.mllib.feature.{HashingTF, IDF, Normalizer, IDFModel}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import java.lang.Math
+import scala.io.Source
 
 
 object VectorSpaceTest {
-
-  def myprint(s: Any) = println("\n>> "+s+" <<\n")
   
-  def norm(v: Vector) = Math.sqrt(v.toArray.map(x => x*x).sum)
+  def norm(v: Vector): Double = Math.sqrt(v.toArray.map(x => x*x).sum)
   
-  def dotProduct(v1: Vector, v2: Vector) = v1.toArray.zip(v2.toArray).map(x => x._1*x._2).sum
+  def dotProduct(v1: Vector, v2: Vector): Double = v1.toArray.zip(v2.toArray).map(x => x._1*x._2).sum
   
-  def cosSimilarity(v1: Vector, v2: Vector) = dotProduct(v1, v2)/(norm(v1)*norm(v2))
+  def cosSimilarity(v1: Vector, v2: Vector): Double = dotProduct(v1, v2)/(norm(v1)*norm(v2))
   
   
   /**
@@ -35,50 +35,66 @@ object VectorSpaceTest {
   * @author Matteo Filipponi
   */  
   def main(args: Array[String]) {
-
-    val sc = new SparkContext("local", "shell")
+  	val sc = new SparkContext("local", "shell")
 
     val crosswords = sc.jsObjectFile("../data/crosswords/*").map(_._2)
     
     val words = Bags.clues(crosswords).distinct // RDD[(String, String)]
-    //val indexedWords = words.zipWithUniqueId.map(x => (x._2, x._1)) // RDD[(Long, (String, String))]
+    val indexedWordsDef = words.zipWithUniqueId.map(x => (x._2, x._1)) // RDD[(Long, (String, String))]
+    val definitions = indexedWordsDef.map(x => (x._1, Stem.clean(x._2._2))) // RDD[(Long, Seq[String])]
     
-    val definitions = words.map(x => x._2.split(" ").toSeq) // RDD[Seq[String]]
-    //val indexedDefinitions = indexedWords.map(x => (x._1, x._2._2.split(" ").toSeq)) // RDD[(Long, Seq[String])]
+    val numberOfWords = definitions.flatMap(_._2).distinct.count
+    println(">> # = "+numberOfWords)
     
     // For a good mapping of the term-frequency hashing function, the number of words of the whole dictionary need to be estimated
-    val vocabularySizeEstim: Int = 10000
-    val hashingTF = new HashingTF(vocabularySizeEstim)
-	val tf: RDD[Vector] = hashingTF.transform(definitions)
-	val idf = new IDF().fit(tf)
+	//val vocabularySizeEstim: Int = 1000
+	val hashingTF = new HashingTF(numberOfWords.toInt)
+	val tf: RDD[Vector] = hashingTF.transform(definitions.values)
+	val idf: IDFModel = new IDF().fit(tf)
 	tf.cache
 	val tfidf: RDD[Vector] = idf.transform(tf)
-	
-	// For this example the query is the first definition of the corpus
-	val query = tfidf.first
+	val definitionsTFIDF = definitions.zip(tfidf).map(x => (x._1._1, x._2)) // RDD[(Long, Vector)]
 	
 	// The normalizer normalizes the vectors, this way the cosine similarity of two vectors is just their dot product
 	val normalizer = new Normalizer()
-	val tfidfNormed = tfidf.map(normalizer.transform)
+	val definitionsTFIDFNormed = definitionsTFIDF.mapValues(normalizer.transform) // RDD[(Long, Vector)]
+  	
+  	print(">> Enter a query: ")
+  	for (line <- Source.stdin.getLines) {
+
+  	val tmp: RDD[Vector] = sc.parallelize(Array(hashingTF.transform(Stem.clean(line))))
+	val query = normalizer.transform(idf.transform(tmp)).first
 	
-	val sims = tfidfNormed.map(v => dotProduct(query, v)) // RDD[Double]
-	val indexedSims = sims.zipWithIndex // (similarity, index): RDD[(Double, Long)]
+	val sims = definitionsTFIDFNormed.map(x => (x._1, dotProduct(query, x._2))) // RDD[(Long, Double)]
 	
 	// Sorting is performed first locally
 	val k = 10
-	val partialTopK = indexedSims.mapPartitions(it => {
+	val partialTopK = sims.mapPartitions(it => {
              val a = it.toArray
-             a.sortBy(-_._1).take(k).iterator
+             a.sortBy(-_._2).take(k).iterator
            }, true)
     
     val collectedTopK = partialTopK.collect
     
-    val topK = collectedTopK.sortBy(-_._1).take(k)
+    val topK = collectedTopK.sortBy(-_._2).take(k) // RDD[(Long, Double)]
 	
 	// Printing the top-k results
-	val collectedWords = words.collect
-	for (c <- topK) myprint(collectedWords(c._2.toInt)._2)
-    
+	val collectedWords = indexedWordsDef.collect.toMap // Map[(Long -> (String, String))]
+	println(">> Query = "+line)
+	for (x <- topK) {
+		println
+		println("Sim = "+x._2)
+		val wordDef = collectedWords.get(x._1) match {
+      		case Some(s) => s
+      		case None => ("?", "?")
+    	}
+    	println("Word = "+wordDef._1)
+		println("Def = "+wordDef._2)
+	}
+    }
 	sc.stop
-  }	
+  }
 }
+
+
+
