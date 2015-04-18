a6392f6a0a8cccae4d9876749eac0f470546ec4d
Laurent Valette <lau.valette@gmail.com>
2015-04-18 12:43:24 +0200

    Add utility methods to manipulate MLlib Vector and SparseVector

--- app/src/main/scala/crosswords/util/Vec.scala
+++ app/src/main/scala/crosswords/util/Vec.scala
@@ -1,6 +1,9 @@
 
 package crosswords.util
 
+import org.apache.spark.mllib.linalg.SparseVector
+import org.apache.spark.mllib.linalg.Vector
+
 /**
  * @author Johan Berdat
  */
@@ -60,4 +63,19 @@ object Vec {
     case West => Vec(-1, 0)
   }
 
+  def dotProductSparse(v1: SparseVector, v2: SparseVector): Double = {
+    // TODO: Since indices are in increasing order, we could decrease the time complexity of this method if needed
+    val products = for {
+      index1 <- v1.indices
+      index2 <- v2.indices
+      if index1 == index2
+    } yield v1(index1) * v2(index2)
+    products.sum
+  }
+  def normSparse(v: SparseVector): Double = Math.sqrt(v.values.foldLeft(0.0){case (accu, value) => accu + value * value})
+  def cosSimilaritySparse(v1: SparseVector, v2: SparseVector): Double = dotProductSparse(v1, v2) / (normSparse(v1) * normSparse(v2))
+
+  def dotProduct(v1: Vector, v2: Vector): Double = v1.toArray.zip(v2.toArray).map(x => x._1 * x._2).sum
+  def norm(v: Vector): Double = Math.sqrt(v.toArray.map(x => x*x).sum)
+  def cosSimilarity(v1: Vector, v2: Vector): Double = dotProduct(v1, v2)/(norm(v1)*norm(v2))
 }
