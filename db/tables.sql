512feaa463a4262c3cc655862709fb804750893d
Jojo le Barjos <jojolebarjos@gmail.com>
2015-04-25 14:42:19 +0200

    fix indentation

--- db/tables.sql
+++ db/tables.sql
@@ -1,9 +1,9 @@
 
 -- Dictionary
 create table Words (
-    wid int not null,
-    word varchar(50) not null,
-    primary key (wid),
+	wid int not null,
+	word varchar(50) not null,
+	primary key (wid),
 	unique index (word)
 );
 
@@ -25,34 +25,34 @@ create table Neighbors (
 
 -- Crosswords infos
 create table Crosswords (
-    cwid int not null,
-    source varchar(50),
-    lang varchar(5),
-    title varchar(50),
-    url varchar(100),
-    author varchar(50),
-    cwdate date,
-    difficulty int,
-    primary key (cwid)
+	cwid int not null,
+	source varchar(50),
+	lang varchar(5),
+	title varchar(50),
+	url varchar(100),
+	author varchar(50),
+	cwdate date,
+	difficulty int,
+	primary key (cwid)
 );
 
 -- Crosswords items
 create table Items (
-    cwid int not null,
-    wid int not null,
-    xcoord int not null,
-    ycoord int not null,
-    clue varchar(100) not null,
-    direction varchar(10) not null,
-    primary key (cwid, xcoord, ycoord, direction),
+	cwid int not null,
+	wid int not null,
+	xcoord int not null,
+	ycoord int not null,
+	clue varchar(100) not null,
+	direction varchar(10) not null,
+	primary key (cwid, xcoord, ycoord, direction),
 	index (cwid),
-    foreign key (cwid)
-        references Crosswords(cwid)
-        on update cascade
+	foreign key (cwid)
+		references Crosswords(cwid)
+		on update cascade
 		on delete cascade,
-    foreign key (wid)
-        references Words(wid)
-        on update cascade
+	foreign key (wid)
+		references Words(wid)
+		on update cascade
 		on delete cascade
 );
 
